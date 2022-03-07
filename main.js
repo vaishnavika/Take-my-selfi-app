6e6a7fdc4bd5749314edd0abc6610ec211f28489
vaishnavika <77280301+vaishnavika@users.noreply.github.com>
2022-03-07 18:37:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var SpeechRecognition=window.webkitSpeechRcognition;
+var recognition=new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+
+}
+recognition.onresult=function(event){
+    console.log(event);
+    var Content=event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML=Content;
+    if(Content =="take my selfie")
+     { console.log("taking selfie --- ");
+     speak(); }
+}
+
+function speak(){
+    var synth=windows.speechSynthesis;
+
+    speak_data= document.getElementById("textbox").value;
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+
+Webcam.set({
+    width:360,
+    hight:250,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");

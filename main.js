044464a9e814b33269905d911ee9e5996b128875
vaishnavika <77280301+vaishnavika@users.noreply.github.com>
2022-03-07 18:47:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
-var SpeechRecognition=window.webkitSpeechRcognition;
-var recognition=new SpeechRecognition();
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
 
-function start(){
-    document.getElementById("textbox").innerHTML="";
+function start() {
+    document.getElementById("textbox").innerHTML = "";
     recognition.start();
 
 }
-recognition.onresult=function(event){
+recognition.onresult = function (event) {
     console.log(event);
-    var Content=event.results[0][0].transcript;
+    var Content = event.results[0][0].transcript;
     console.log(Content);
-    document.getElementById("textbox").innerHTML=Content;
-    if(Content =="take my selfie")
-     { console.log("taking selfie --- ");
-     speak(); }
+    document.getElementById("textbox").innerHTML = Content;
+    if (Content == "take my selfie") {
+        console.log("taking selfie --- ");
+        speak();
+    }
 }
 
-function speak(){
-    var synth=windows.speechSynthesis;
+function speak() {
+    var synth = window.speechSynthesis;
 
-    speak_data= document.getElementById("textbox").value;
-var utterThis=new SpeechSynthesisUtterance(speak_data);
-synth.speak(utterThis);
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
 }
 
 Webcam.set({
-    width:360,
-    hight:250,
-    image_format:'png',
-    png_quality:90
+    width: 360,
+    hight: 250,
+    image_format: 'png',
+    png_quality: 90
 });
-camera=document.getElementById("camera");
+camera = document.getElementById("camera");

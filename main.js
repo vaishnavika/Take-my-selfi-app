b16bdb52779b34ae370c6f67e0669415218f9dd6
vaishnavika <77280301+vaishnavika@users.noreply.github.com>
2022-03-14 18:41:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,9 +20,18 @@ recognition.onresult = function (event) {
 function speak() {
     var synth = window.speechSynthesis;
 
+    speak_data = "Taking your selfie in 5 seconds";
+
     speak_data = document.getElementById("textbox").value;
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+     } , 5000);
+    
 }
 
 Webcam.set({
@@ -32,3 +41,17 @@ Webcam.set({
     png_quality: 90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+}
+
